b04b5a61808b5c6af041afb18d5cc05b7f7c17ef
Nitin <nitingakwad07@gmail.com>
2026-03-20 23:34:02 +0530

    ''

--- src/main/java/com/example/deploy/DeployApplication.java
+++ src/main/java/com/example/deploy/DeployApplication.java
@@ -2,12 +2,18 @@ package com.example.deploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+    }
+)
 public class DeployApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DeployApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DeployApplication.class, args);
+    }
 }
